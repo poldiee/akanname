20a2880d6da21b3e907c38fa31187618566fa534
poldiee <nessidave@gmail.com>
2022-03-04 17:15:43 +0300

    change to function calculateDay()-->calcDay()

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function validate() {
     }
 }
 
-function calculateDay(){
+function calcDay(){
 year = document.getElementById("year").value;
 CC = parseInt(year.substring(0,2));
 YY = parseInt(year.substring(2,4));
@@ -34,3 +34,4 @@ d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
 console.log(d);
 return (Math.floor(d));
 }
+
