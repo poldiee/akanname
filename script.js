26e2ab16f5b8b4e22fc33d375af59b2f5de118c6
poldiee <nessidave@gmail.com>
2022-03-04 14:59:16 +0300

    parameters input js

---
+++ script.js
@@ -0,0 +1,4 @@
+var CC, YY, MM, DD, d, dayVal;
+var dayNme = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleNme = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var femaleNme = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
