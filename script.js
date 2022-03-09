cd6db41c153fa9cdecadb251ca2315385cf42ddb
poldiee <nessidave@gmail.com>
2022-03-09 11:19:55 +0300

    year2020

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 function validate() {
   var genders = document.getElementsByName("gender");
   if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1900) {
-     alert( "Please provide a valid year of birth! eg 2019" );
+     alert( "Please provide a valid year of birth! eg 2020" );
      document.myForm.year.focus() ;
      return false;
   }
